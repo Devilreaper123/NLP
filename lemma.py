f89d030d8f0878ba85e5b1ca4f763f6f7cc594c2
Devilreaper123 <ronitshahu@gmail.com>
2022-10-28 16:52:46 +0530

    Added Lemma.py for practical Implementation

---
+++ lemma.py
@@ -0,0 +1,16 @@
+import spacy
+import spacy_transformers
+nlp = spacy.load('en_core_web_trf')
+doc = nlp("Knowing jack, he was surely trying to do something stupid")
+for token in doc:
+    print(token.text , token.lemma_)
+
+
+with open('wiki_us.txt') as f:
+    text = f.read()
+
+doc = nlp(text)
+
+for token in doc:
+    print(token.text ,"------>",token.lemma_)
+
