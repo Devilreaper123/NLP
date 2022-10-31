88b29e3866563c412a1183b6793f7019c044ac28
Devilreaper123 <ronitshahu@gmail.com>
2022-10-31 16:48:29 +0530

    Added wordnet.py

---
+++ wordnet.py
@@ -0,0 +1,18 @@
+import nltk
+nltk.download('wordnet')
+import spacy
+from spacy_wordnet.wordnet_annotator import WordnetAnnotator 
+
+# Load a spaCy model (supported languages are "es" and "en") 
+nlp = spacy.load('en_core_web_sm')
+# spaCy 3.x
+nlp.add_pipe("spacy_wordnet", after='tagger')
+# spaCy 2.x
+# nlp.add_pipe(WordnetAnnotator(nlp.lang), after='tagger')
+token = nlp('prices')[0]
+
+# WordNet object links spaCy token with NLTK WordNet interface by giving access to
+# synsets 
+print(token._.wordnet.synsets())
+print('\n')
+print(token._.wordnet.lemmas())
